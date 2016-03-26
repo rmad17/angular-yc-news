940ac5a22834bfc6864d215e069d470266f1d904
rmad17 <souravbasu17@gmail.com>
2016-03-26 20:51:47 +0530

    Not working.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,10 +4,20 @@
  *
  * Distributed under terms of the MIT license.
  */
-function newStoriesController($scope, $http) {
+var app = angular.module('myApp', []);
+console.log("a");
+app.controller('nSCtrl',['webServices','$scope',function(webServices,$scope){
+        webServices.getnewStories().then(function(response){
+            $scope.items = response; //Assign data received to $scope.data
+    console.log("b");
+        });
+    }]);
+/*app.controller('nSController', function($scope, $http) {
     var url = "https://hacker-news.firebaseio.com/v0/newstories";
 
+    console.log("b");
     $http.get(url).success( function(response) {
        $scope.items = response;
     });
-}
+});*/
+console.log("c");
--- app/js/services.js
+++ app/js/services.js
@@ -4,3 +4,14 @@
  *
  * Distributed under terms of the MIT license.
  */
+var myApp = angular.module('myApp', []);
+//a factory to consume webservices and return data to controllers.
+    myApp.factory('webServices',['$http',function($http){
+        return {
+            getnewStories : function(){
+                return  $http.get('https://hacker-news.firebaseio.com/v0/newstories').then(function(response){ //wrap it inside another promise using then
+                            return response.data;  //only return friends
+                        });
+            }
+        }
+    }]);
