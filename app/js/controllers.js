a6667678053c5732cde6c5a907584587f53bdd18
rmad17 <souravbasu17@gmail.com>
2016-03-28 13:51:59 +0530

    Improved menus

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,20 +5,3 @@
  * Distributed under terms of the MIT license.
  */
 //var app = angular.module('myApp', []);
-console.log("a");
-/*
-angular.module('myApp').controller('nSCtrl',['webServices','$scope',function(webServices,$scope){
-        webServices.getnewStories().then(function(response){
-            $scope.items = response; //Assign data received to $scope.data
-    console.log("b");
-        });
-    }]);
-/*app.controller('nSController', function($scope, $http) {
-    var url = "https://hacker-news.firebaseio.com/v0/newstories";
-
-    console.log("b");
-    $http.get(url).success( function(response) {
-       $scope.items = response;
-    });
-});*/
-console.log("c");
