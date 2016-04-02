85252f3eae8dfb39d989673a82590df7eee64453
rmad17 <souravbasu17@gmail.com>
2016-04-02 17:41:38 +0530

    Organised code

--- app/app.js
+++ app/app.js
@@ -13,14 +13,14 @@ config(['$routeProvider', function($routeProvider) {
 }]);
 */
 (function(){
-    var test;
-    angular.module('myApp',[
+    var app = angular.module('myApp',[
     'ui.bootstrap',
     'myApp.view1',
     'myApp.view2',
-    'myApp.version']).
+    'myApp.version']);
 
-    config(['$routeProvider', function($routeProvider) {
+    app
+    .config(['$routeProvider', function($routeProvider) {
       $routeProvider.
         when('/view1', {
             templateUrl: 'view1/view1.html',
@@ -67,35 +67,5 @@ config(['$routeProvider', function($routeProvider) {
         });
     }])
 
-    // Top Stories
-    .factory('tsFactory',['$http',function($http){
-        var ids = [];
-        var tsList = [];
-        return {
-            getTopStories : function(){
-                return  $http.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty').then(function(response){ //wrap it inside another promise using then
-                            ids = response.data;
-                            var getStoryDetails = function(id){
-                                var u1 = 'https://hacker-news.firebaseio.com/v0/item/';
-                                var u2 = id.toString();
-                                var u3 = '.json?print=pretty';
-                                var url = u1 + u2 + u3;
-                                return  $http.get(url).then(function(response) { //wrap it inside another promise using then
-                                        tsList.push(response.data);
-                                        return response.data;  //only return friends
-                                });
-                            };
-                            for(var i = 0; i < 20; i++){
-                                getStoryDetails(ids[i]);
-                            }
-                            return tsList;
-                });
-        }}}])
-    //define controller and inject webServices service as dependency.
-    .controller('tsCtrl',['tsFactory','$scope',function(tsFactory, $scope){
-        tsFactory.getTopStories().then(function(response){
-            $scope.tsitems = response; //Assign data received to $scope.data
-        });
-    }]);
 
 })();
